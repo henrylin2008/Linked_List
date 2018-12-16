177b666ea48313e2e712b3aa1c7aa3fa914cd74b
Henry Lin <henry.lin@appdirect.com>
2018-12-16 08:56:18 -0800

    Linked List Reversal

---
+++ Linked_List_Reversal.py
@@ -0,0 +1,12 @@
+# Problem¶
+# Write a function to reverse a Linked List in place. The function will take in the head of the list as input and return the new head of the list.
+
+# You are given the example Linked List Node class:
+
+class Node(object):
+    
+    def __init__(self,value):
+        
+        self.value = value
+        self.nextnode = None
+
